9a3c15db04642a847bd4afcc0fe55b12aa54f4fd
aganie15 <aga.niewiadomska@softwire.com>
2022-09-13 09:51:20 +0100

    Completed initial pawn tests

--- lib/Chessington/engine/pieces.rb
+++ lib/Chessington/engine/pieces.rb
@@ -22,13 +22,33 @@ module Chessington
         board.move_piece(current_square, new_square)
       end
 
-      # checks if the piece is obstructed # TODO: add the push to list here too?
-      def obstructed_move?(board, move_square)
-        if board.get_piece(move_square).nil?
-          false
-        else
-          true
+      # checks if the move_square is obstructed in any way
+      # intermediate squares is a list which holds all the squares we have to 'jump' over -
+      # these must be clear for our move to be possible
+      def add_unobstructed_move(board, available_moves, move_square, intermediate_squares = [])
+        no_obstruction = true
+        intermediate_squares.each { |square|
+          unless board.get_piece(square).nil?
+            no_obstruction = false
+          end
+        }
+        if board.get_piece(move_square).nil? && no_obstruction
+          available_moves.push(move_square)
         end
+        available_moves
+      end
+
+      # capture squares is a list containing the possible capture squares
+      def add_capture_moves(board, available_moves, capture_squares, current_player)
+        capture_squares.each { |capture_square|
+          # check if there is a piece here
+          unless board.get_piece(capture_square).nil?
+            capture_piece = board.get_piece(capture_square)
+            if capture_piece.player != current_player
+              available_moves.push(capture_square)
+            end
+          end
+        }
       end
 
     end
@@ -43,26 +63,30 @@ module Chessington
         available_moves = []
         if self.player == Player::WHITE
           if current_square.row == 1
-            # maybe outsource all this junk into a new method
             new_position = Square.at(current_square.row + 2, current_square.column)
-            unless obstructed_move?(board, new_position)
-              available_moves.push(new_position)
-            end
+            # must check we are not jumping over any pieces
+            middle_tile = Square.at(current_square.row + 1, current_square.column)
+            add_unobstructed_move(board, available_moves, new_position, [middle_tile])
           end
-          new_position = Square.at(current_square.row + 1, current_square.column)
-          unless obstructed_move?(board, new_position)
-            available_moves.push(new_position)
+          if current_square.row != 7
+            new_position = Square.at(current_square.row + 1, current_square.column)
+            add_unobstructed_move(board, available_moves, new_position)
+            # check for possible captures
+            capture_squares = [Square.at(current_square.row + 1, current_square.column + 1), Square.at(current_square.row + 1, current_square.column - 1)]
+            add_capture_moves(board, available_moves, capture_squares, self.player)
           end
         else
           if current_square.row == 6
             new_position = Square.at(current_square.row - 2, current_square.column)
-            unless obstructed_move?(board, new_position)
-              available_moves.push(new_position)
-            end
+            middle_tile = Square.at(current_square.row - 1, current_square.column)
+            add_unobstructed_move(board, available_moves, new_position, [middle_tile])
           end
-          new_position = Square.at(current_square.row - 1, current_square.column)
-          unless obstructed_move?(board, new_position)
-            available_moves.push(new_position)
+          if current_square.row != 0
+            new_position = Square.at(current_square.row - 1, current_square.column)
+            add_unobstructed_move(board, available_moves, new_position)
+            # check for possible captures
+            capture_squares = [Square.at(current_square.row - 1, current_square.column + 1), Square.at(current_square.row - 1, current_square.column - 1)]
+            add_capture_moves(board, available_moves, capture_squares, self.player)
           end
         end
         available_moves
